c489b5ccd506110c8ca71241fb7ea86a143ddb32
Almaz Fedorov <fedoro3valmaz@gmail.com>
2026-03-26 00:06:36 +0300

    Implementing the business logic of the reducer

--- cmd/core/main.go
+++ cmd/core/main.go
@@ -4,12 +4,16 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"math/rand"
 	"net"
+	"time"
 
 	pb "github.com/mintrage/linkguard/proto" // Импортируем наш сгенерированный код
 	"github.com/redis/go-redis/v9"
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/reflection" // Понадобится для тестирования
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/reflection"
+	"google.golang.org/grpc/status" // Понадобится для тестирования
 )
 
 // linkServer - наша структура, которая реализует интерфейс из proto-файла
@@ -20,26 +24,58 @@ type linkServer struct {
 
 // 1. Реализация метода CreateLink
 func (s *linkServer) CreateLink(ctx context.Context, req *pb.CreateLinkRequest) (*pb.CreateLinkResponse, error) {
+	originalURL := req.GetOriginalUrl()
+	if originalURL == "" {
+		return nil, status.Errorf(codes.InvalidArgument, "URL не может быть пустым")
+	}
+	fmt.Printf("Пришел запрос на сокращение: %s\n", originalURL)
+	shortCode := generateShortCode(6)
+	err := s.rdb.Set(ctx, shortCode, originalURL, 0).Err()
+	if err != nil {
+		log.Printf("Ошибка при сохранении в Redis: %v", err)
+		return nil, status.Error(codes.Internal, "Не удалось сохранить ссылку")
+	}
 
-	fmt.Printf("Пришел запрос на сокращение: %s\n", req.GetOriginalUrl())
-
-	// Пока БД нет, делаем хардкод-заглушку (mock)
-	fakeShortLink := "go.dev/xyz"
+	fmt.Printf("✅ Создана ссылка: %s -> %s\n", shortCode, originalURL)
 
 	return &pb.CreateLinkResponse{
-		ShortLink: fakeShortLink,
+		ShortLink: shortCode,
 	}, nil
 }
 
 // 2. Реализация метода GetOriginalLink
 func (s *linkServer) GetOriginalLink(ctx context.Context, req *pb.GetOriginalLinkRequest) (*pb.GetOriginalLinkResponse, error) {
-	fmt.Printf("Запрошенный короткий код: %s\n", req.GetShortLink())
+	shortCode := req.GetShortLink()
 
-	fakeOriginalUrl := "https://google.com"
+	if shortCode == "" {
+		return nil, status.Error(codes.InvalidArgument, "Короткий URL не может быть пустым!")
+	}
+	fmt.Printf("Запрошенный короткий код: %s\n", shortCode)
+	originalURL, err := s.rdb.Get(ctx, shortCode).Result()
+	if err == redis.Nil {
+		return nil, status.Error(codes.NotFound, "Ссылка не найдена")
+	} else if err != nil {
+		log.Printf("Ошибка чтения из Redis: %v", err)
+		return nil, status.Error(codes.Internal, "Внутренняя ошибка базы данных")
+	}
+
+	fmt.Printf("🔍 Переход по ссылке: %s -> %s\n", shortCode, originalURL)
 
 	return &pb.GetOriginalLinkResponse{
-		OriginalUrl: fakeOriginalUrl,
+		OriginalUrl: originalURL,
 	}, nil
+}
+
+func generateShortCode(length int) string {
+	const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+
+	b := make([]byte, length)
+	for i := range b {
+		b[i] = charset[r.Intn(len(charset))]
+	}
+	return string(b)
 }
 
 func main() {
